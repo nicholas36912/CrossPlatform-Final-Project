dee31795ad2b47f132f4cc855bc3820d7d77ee02
Nicholas Scinocco <nicholas.scinocco2@gmail.com>
2023-12-04 16:25:46 -0500

    initial

---
+++ MorningMotivationStation/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import AppNavigator from './navigation/AppNavigator';
+import AppNavigation from './AppNavigation';
+
+export default function App() {
+  return <AppNavigation />;
+}
